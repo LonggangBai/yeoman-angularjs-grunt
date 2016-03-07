68a250320277d57eecccf81a6b7e8dca6f5884ca
longgangbai <bailonggang@easyway.net.cn>
2016-03-07 15:05:58 +0800

    init yeoman-angularjs-grunt project

---
+++ app/scripts/app.js
@@ -0,0 +1,14 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name yeomanAngularjsApp
+ * @description
+ * # yeomanAngularjsApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('yeomanAngularjsApp', [
+    'ngAnimate'
+  ]);
